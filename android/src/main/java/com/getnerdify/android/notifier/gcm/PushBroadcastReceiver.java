cad0275709d2dd1b91dbad3abdb99842ce078a84
Hosmel Quintana <hosmelq@gmail.com>
2015-01-26 13:47:08 -0600

    fix: support multi lenguage in notification

--- android/src/main/java/com/getnerdify/android/notifier/gcm/PushBroadcastReceiver.java
+++ android/src/main/java/com/getnerdify/android/notifier/gcm/PushBroadcastReceiver.java
@@ -3,10 +3,15 @@ package com.getnerdify.android.notifier.gcm;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
+import com.getnerdify.android.notifier.R;
 import com.getnerdify.android.notifier.ui.CodeActivity;
 import com.parse.ParsePushBroadcastReceiver;
 import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PushBroadcastReceiver extends ParsePushBroadcastReceiver {
 
@@ -20,6 +25,15 @@ public class PushBroadcastReceiver extends ParsePushBroadcastReceiver {
     @Override
     protected void onPushReceive(Context context, Intent intent) {
         if (ParseUser.getCurrentUser() == null) {
+            return;
+        }
+
+        try {
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+            json.put("alert", String.format(context.getString(R.string.notification_code), json.getString("code")));
+            intent.putExtra("com.parse.Data", json.toString());
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
             return;
         }
 
